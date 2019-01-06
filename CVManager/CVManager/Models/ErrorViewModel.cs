53b5048ef7cd623e70fdd390f8989d69f260083a
SiwyKrzysiek <k.fajny@gmail.com>
2019-01-06 19:09:25 +0100

    Create project

---
+++ CVManager/CVManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CVManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
