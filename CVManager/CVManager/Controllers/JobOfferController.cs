4985d0e1b8e576418d356c7ec6f8cbdffe6f4828
01133318@pw.edu.pl <01133318@pw.edu.pl>
2019-01-07 10:08:51 +0100

    Pass example job offer list to the view

--- CVManager/CVManager/Controllers/JobOfferController.cs
+++ CVManager/CVManager/Controllers/JobOfferController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CVManager.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,11 +11,19 @@ namespace CVManager.Controllers
 {
     public class JobOfferController : Controller
     {
+        private static List<JobOffer> _jobOffers = new List<JobOffer>
+        {
+            new JobOffer {Id = 1, JobTitle = "C# Programmer"},
+            new JobOffer {Id = 2, JobTitle = "Backend Developer"},
+            new JobOffer {Id = 3, JobTitle = "Android Developer"},
+            new JobOffer {Id = 4, JobTitle = "Teacher"},
+            new JobOffer {Id = 5, JobTitle = "Firefighter"}
+        };
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            //TODO: Dispaly all job offers
-            return View();
+            return View(_jobOffers);
         }
     }
 }
