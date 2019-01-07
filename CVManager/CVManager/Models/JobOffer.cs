e3821d8df9cf2d73dea76b970c6086d91d90b7a9
01133318@pw.edu.pl <01133318@pw.edu.pl>
2019-01-07 10:36:33 +0100

    More detailed model

--- CVManager/CVManager/Models/JobOffer.cs
+++ CVManager/CVManager/Models/JobOffer.cs
@@ -6,5 +6,8 @@ namespace CVManager.Models
     {
         public int Id { get; set; }
         public string JobTitle { get; set; }
+        public decimal MonthlySalayr { get; set; }
+        public string Requirements { get; set; }
+        public string Location { get; set; }
     }
 }
