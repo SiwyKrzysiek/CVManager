4a1dab4e7dfaf05c574d87810e73b19baf111012
01133318@pw.edu.pl <01133318@pw.edu.pl>
2019-01-07 10:47:06 +0100

    Fix synax error

--- CVManager/CVManager/Controllers/JobOfferController.cs
+++ CVManager/CVManager/Controllers/JobOfferController.cs
@@ -14,7 +14,7 @@ namespace CVManager.Controllers
         private static List<JobOffer> _jobOffers = new List<JobOffer>
         {
             new JobOffer {Id = 1, JobTitle = "C# Programmer", MonthlySalayr = 6800, Location="Warszawa", Requirements="Knowlege of C#"},
-            new JobOffer {Id = 2, JobTitle = "Backend Developer", MonthlySalayr= 5 495, Location="Gdańsk", Requirements="Expirience in PHP"},
+            new JobOffer {Id = 2, JobTitle = "Backend Developer", MonthlySalayr= 5495, Location="Gdańsk", Requirements="Expirience in PHP"},
             new JobOffer {Id = 3, JobTitle = "Android Developer", Requirements="Java and Kotlin"},
             new JobOffer {Id = 4, JobTitle = "Teacher", Location="Piaseczno", Requirements="Degree in history"},
             new JobOffer {Id = 5, JobTitle = "Firefighter", Location="Warszawa"}
