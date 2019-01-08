1bc33eaef538d960618393c71c4c34543e3d8551
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-08 23:58:07 +0100

    Controler for handling http status codes

---
+++ CVManager/CVManager/Controllers/HttpStatusCodeResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVManager.Controllers
+{
+    internal class HttpStatusCodeResult : ActionResult
+    {
+        private object badRequest;
+
+        public HttpStatusCodeResult(object badRequest)
+        {
+            this.badRequest = badRequest;
+        }
+    }
+}
