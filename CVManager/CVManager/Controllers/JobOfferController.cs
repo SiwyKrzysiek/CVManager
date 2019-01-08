37f2220691c3f36ba98c8e417e67682220bd0e39
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-08 22:29:08 +0100

    Sample jobs that have new filelds filed

--- CVManager/CVManager/Controllers/JobOfferController.cs
+++ CVManager/CVManager/Controllers/JobOfferController.cs
@@ -1,23 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CVManager.Models;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CVManager.Controllers
 {
     public class JobOfferController : Controller
     {
-        private static List<JobOffer> _jobOffers = new List<JobOffer>
+        private static readonly List<Company> _companies = new List<Company>()
         {
-            new JobOffer {Id = 1, JobTitle = "C# Programmer", MonthlySalary = 6800, Location="Warszawa", Requirements="Knowlege of C#"},
-            new JobOffer {Id = 2, JobTitle = "Backend Developer", MonthlySalary= 5495, Location="Gdańsk", Requirements="Expirience in PHP"},
-            new JobOffer {Id = 3, JobTitle = "Android Developer", Requirements="Java and Kotlin"},
-            new JobOffer {Id = 4, JobTitle = "Teacher", Location="Piaseczno", Requirements="Degree in history"},
-            new JobOffer {Id = 5, JobTitle = "Firefighter", Location="Warszawa"}
+            new Company() {Id = 1, Name = "Predica"},
+            new Company() {Id = 2, Name = "Microsoft"},
+            new Company() {Id = 3, Name = "Github"},
+            new Company() {Id = 4, Name = "Havi Logistics"},
+            new Company() {Id = 5, Name = "Sweet Home"}
+        };
+
+        private static readonly List<JobOffer> _jobOffers = new List<JobOffer>
+        {
+            new JobOffer
+            {
+                Id = 1,
+                JobTitle = "C# Programmer",
+                Company = _companies.FirstOrDefault(c => c.Name == "Precica"),
+                Created = DateTime.Now.AddDays(-5),
+                SalaryFrom = 3500,
+                SalaryTo = 6800,
+                Location = "Cracow",
+                Description = "Experienced C# developer with electronic background. The main task would be building smart devices software.",
+                ValidUntil = DateTime.Now.AddDays(30)
+            },
+            new JobOffer{
+                Id = 2,
+                JobTitle = "Frontend Developer",
+                Company = _companies.FirstOrDefault(c => c.Name =="Microsoft"),
+                Created = DateTime.Now.AddDays(-2),
+                Description = "Developing Office 365 front end interface. Working with SharePoint and graph API. Connecting with AAD and building ML for Mailbox smart assistant.",
+                Location = "Poland",
+                SalaryFrom = 2000,
+                SalaryTo = 10000,
+                ValidUntil = DateTime.Now.AddDays(20)
+            },
+            new JobOffer
+            {
+                Id = 3,
+                JobTitle = "Baker",
+                Company = _companies.FirstOrDefault(c => c.Name == "Sweet Home"),
+                Created = DateTime.Now.AddHours(-8),
+                SalaryFrom = 1500,
+                SalaryTo = 4000,
+                Location = "Warsaw",
+                Description = "Baker in newly opened bakery. Baking tasty cakes and cokes. Experience with home made ice cram would be an additional benefit",
+                ValidUntil = DateTime.Now.AddDays(10)
+            }
         };
 
         // GET: /<controller>/
