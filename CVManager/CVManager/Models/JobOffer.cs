d7103aa58e618814df390b9dd441212edd711ce4
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-08 11:20:14 +0100

    More fields and requirements in JobOffer

--- CVManager/CVManager/Models/JobOffer.cs
+++ CVManager/CVManager/Models/JobOffer.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CVManager.Models
 {
-    public class JobOffer
+    public class JobOffer //Some of the fields are virtual in example but I don't know why. Let's see what happens if they are not :D
     {
         public int Id { get; set; }
+        [Display(Name = "Job title")]
+        [Required]
         public string JobTitle { get; set; }
-        public decimal MonthlySalary { get; set; }
-        public string Requirements { get; set; }
+        public Company Company { get; set; }
+        public int CompanyId { get; set; }
+        [Display(Name = "Salary from")]
+        public decimal? SalaryFrom { get; set; }
+        [Display(Name = "Salary to")]
+        public decimal? SalaryTo { get; set; }
+        public DateTime Created { get; set; }
         public string Location { get; set; }
+        [Required]
+        [MinLength(50)]
+        public string Description { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyy-MM-dd}")]
+        [Display(Name = "Valid until")]
+        public DateTime? ValidUntil { get; set; }
     }
 }
