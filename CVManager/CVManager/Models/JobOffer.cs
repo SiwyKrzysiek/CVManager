4b5f4742d756ccb1f2e0a4d915ed23783081eb12
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-08 11:05:26 +0100

    Typo fix

--- CVManager/CVManager/Models/JobOffer.cs
+++ CVManager/CVManager/Models/JobOffer.cs
@@ -6,7 +6,7 @@ namespace CVManager.Models
     {
         public int Id { get; set; }
         public string JobTitle { get; set; }
-        public decimal MonthlySalayr { get; set; }
+        public decimal MonthlySalary { get; set; }
         public string Requirements { get; set; }
         public string Location { get; set; }
     }
