d6b1452549ff69cbf7842607f8e640d2ef82b1ef
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-09 22:58:53 +0100

    Aplication action

---
+++ CVManager/CVManager/Controllers/ApplicationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CVManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVManager.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class ApplicationController : Controller
+    {
+        public IActionResult Apply(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var offer = JobOfferController._jobOffers.Find(o => o.Id == id);
+            if (offer == null)
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+            return View(offer);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Apply(JobApplication model)
+        //{
+            
+        //}
+    }
+}
