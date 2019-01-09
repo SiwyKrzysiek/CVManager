402ab123cfe1244f988c5c81a4a48a24c0d5ef6b
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-09 23:30:17 +0100

    Add date in job aplication

--- CVManager/CVManager/Models/JobApplication.cs
+++ CVManager/CVManager/Models/JobApplication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,17 @@ namespace CVManager.Models
     {
         public int Id { get; set; }
         public int OfferId { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string EmailAddress { get; set; }
         public bool ContactAgreement { get; set; }
         public string CvUrl { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyy-MM-dd}")]
+        [Display(Name = "Date of Birth")]
+        public DateTime? DateOfBirth { get; set; }
     }
 }
