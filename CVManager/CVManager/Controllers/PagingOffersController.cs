42dac106c06354a668a748a27a1cf157c25b399c
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-12 23:20:10 +0100

    Paging api controler get request

--- CVManager/CVManager/Controllers/PagingOffersController.cs
+++ CVManager/CVManager/Controllers/PagingOffersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CVManager.EntityFramework;
+using CVManager.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,7 +24,29 @@ namespace CVManager.Controllers
         [HttpGet("{pageNumber}", Name = "Get")]
         public IActionResult Get(int pageNumber = 1)
         {
-            return Ok(new {id = pageNumber, name = "Stefan"});
+            const int pageSize = 2;
+            int recordCount = _context.JobOffers.Count();
+            int pagesCount = (int)Math.Ceiling((double) recordCount / pageSize);
+
+            if (pageNumber > pagesCount || pageNumber < 1)
+                return BadRequest();
+
+            var offers = LoadJobOffers().Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+
+
+            return Ok(new JobOffersPagingView() {JobOffers = offers, PagesCount = pagesCount});
+        }
+
+        private List<JobOffer> LoadJobOffers()
+        {
+            var jobOffers = _context.JobOffers.ToList();
+            var companies = _context.Companies.ToList();
+            foreach (var offer in jobOffers)
+            {
+                offer.Company = companies.FirstOrDefault(c => c.Id == offer.CompanyId);
+            }
+
+            return jobOffers;
         }
     }
 }
