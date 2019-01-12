f59203d1b26eb2ebf61033f175319719d614fc76
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-12 22:57:31 +0100

    Paging contorler

---
+++ CVManager/CVManager/Controllers/PagingOffersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CVManager.EntityFramework;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagingOffersController : ControllerBase
+    {
+        private DataContext _context;
+
+        public PagingOffersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PagingOffers/5
+        [HttpGet("{pageNumber}", Name = "Get")]
+        public IActionResult Get(int pageNumber = 1)
+        {
+            return Ok(new {id = pageNumber, name = "Stefan"});
+        }
+    }
+}
