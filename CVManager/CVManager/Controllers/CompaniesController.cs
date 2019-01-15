f1cebc6b464ce3e611b58010f5d9b2d73f6eae33
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-15 12:43:18 +0100

    API for gettin list of companies

---
+++ CVManager/CVManager/Controllers/CompaniesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CVManager.EntityFramework;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private DataContext _context;
+
+        public CompaniesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get all companies that can post job offers
+        /// </summary>
+        /// <returns>List of companies</returns>
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            var companies = _context.Companies.ToList();
+
+            return Ok(companies);
+        }
+    }
+}
