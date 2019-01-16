5262cbb4090b8e5d2fdc30fbfd6ee8fe886c0ea0
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-16 19:59:19 +0100

    Enter tokens from Azure

--- CVManager/CVManager/Controllers/SessionController.cs
+++ CVManager/CVManager/Controllers/SessionController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AADB2C.Controllers;
+//using AADB2C.Controllers;
 using CommunityCertForT.Models;
 using CVManager.Controllers;
 using Microsoft.AspNetCore.Authentication;
