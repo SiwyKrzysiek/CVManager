95170bc2aabe927648984b334fc8bbcc2d04af98
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-17 22:24:38 +0100

    Try to add file input

--- CVManager/CVManager/Models/JobApplicationCrateView.cs
+++ CVManager/CVManager/Models/JobApplicationCrateView.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CVManager.Models
 {
     public class JobApplicationCrateView : JobApplication
     {
         public string JobTitle { get; set; }
+        [BindProperty]
+        public IFormFile Photo { get; set; }
     }
 }
