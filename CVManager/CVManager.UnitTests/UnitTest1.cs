7bc839f31a45ed9e9bad3bf2ca0ce5e505beb729
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-01-24 10:39:18 +0100

    Attempt to mock data base context

--- CVManager/CVManager.UnitTests/UnitTest1.cs
+++ CVManager/CVManager.UnitTests/UnitTest1.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Data.Entity;
+using CVManager.Controllers;
+using CVManager.EntityFramework;
+using CVManager.Models;
 using NUnit.Framework;
+using Moq;
 
 namespace Tests
 {
@@ -10,8 +16,15 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void Test1() //See if controllers tries to access db
         {
+            var mocDB = new Mock<DataContext>();
+            mocDB.SetupAllProperties();
+            //mocDB.SetupGet(db => db.JobOffers);
+
+            var controler = new OffersController(mocDB.Object);
+            //mocDB.VerifyGet(db => db.JobOffers);
+
             Assert.Pass();
         }
     }
